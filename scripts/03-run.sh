f42fd9030659da684879feeb95e4e3ece73598ff
Zain <zmathews@student.wethinkcode.co.za>
2025-07-31 13:58:40 +0200

    forgot the nginx service spin up

--- scripts/03-run.sh
+++ scripts/03-run.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "$0")/.."
 
 echo "Starting containers..."
-docker compose up -d db app
+docker compose up -d db app nginx
 
 echo "Checking and seeding player data"
 docker compose run --rm seed
